fd4ff9e21bd250a1627553e3811c0c75c98b546e
bayazouni <132158880+bayazouni@users.noreply.github.com>
2023-04-30 07:26:01 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+﻿function addComment() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var comment = document.getElementById("comment").value;
+
+  var newComment = "<p><strong>" + name + ":</strong> " + comment + "</p>";
+
+  var commentsList = document.getElementById("comments-list");
+  commentsList.innerHTML += newComment;
+
+  document.getElementById("comment-form").reset();
+}
